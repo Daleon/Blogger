ebe83148a6f70dddc71b22e8fbf1880863e49252
Daleon <rugfia@yahoo.com>
2016-12-10 16:31:10 -0700

    First Commit For I-1

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,5 +4,28 @@ class ArticlesController < ApplicationController
   end
 
   def show
+    @articles = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article.find(params[id]).destroy
+    redirect '/articles'
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
   end
 end
